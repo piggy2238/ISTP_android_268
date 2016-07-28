ace254b0c88eb505a709ce9ae377b4ca3f0cb808
Ooo工友ooO <r00621109@ntu.edu.tw>
2016-07-28 12:29:11 +0800

    Create PokemonListActivity and listLayout file

---
+++ app/src/main/java/com/example/user/myapplication/PokemonListActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 2016/7/28.
+ */
+public class PokemonListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
