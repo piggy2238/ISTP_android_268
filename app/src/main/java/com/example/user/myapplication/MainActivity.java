27a4ac9154b2a4f42c23384ab0f1e7608ea44a1e
Ooo工友ooO <r00621109@ntu.edu.tw>
2016-08-02 12:28:58 +0800

    0802 MainActivity Add Comment

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,8 +19,9 @@ import com.example.user.myapplication.model.PokemonInfo;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,RadioGroup.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,RadioGroup.OnCheckedChangeListener,TextView.OnEditorActionListener {
 
+//  設定使用變數:類別 名稱與內容
     TextView infoText;
     RadioGroup optionGrp;
     EditText name_editText;
@@ -27,12 +29,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String[] pokemonNames = new String[]{
         "小火龍","傑尼龜","妙蛙種子"
     };
-
+//  程式初始化
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//選擇要使用的Layout
 
+        //透過id設定各Activity中的物件與xml的物件結合
+        //如有需要根據各項目需求要設定其listener
         Button confirm_button = (Button)findViewById(R.id.confirm_button);
         confirm_button.setOnClickListener(this);
 
@@ -41,37 +45,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         infoText = (TextView) findViewById(R.id.infoText);
         name_editText = (EditText) findViewById(R.id.name_editText);
+        name_editText.setOnEditorActionListener(this);
 
     }
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+    //按下確認後
+    //1. 顯示歡迎介面
+    //2. 跳轉至下一頁面
+    //  發生click事件要做的事情
     @Override
     public void onClick(View v) {
         int viewId = v.getId();
         if(viewId == R.id.confirm_button) {
 
-            int changeActivityInSecs = 5;
+            int changeActivityInSecs = 5; //延遲進入的秒數
+            //1. 顯示歡迎介面
             infoText.setText(String.format("你好, 訓練家%s 歡迎來到神奇寶貝的世界,你的夥伴是%s,冒險即將於%d秒鐘之後開始",
                     name_editText.getText().toString(),
                     pokemonNames[selectedOptionIndex],
                     changeActivityInSecs
                     ));
-
-            //切換到另一個Activity
-                //延遲n秒進入
-                //
+            //2. 跳轉至下一頁面 //切換到另一個Activity
+            //新增一個跳轉的物件
                 Handler handler = new Handler(MainActivity.this.getMainLooper());
-//                handler.postDelayed("一個run的介面<function>","延遲 n 毫秒")
+//              handler.postDelayed("Runnable","delay time")
+//              handler.postDelayed("要做的事情","延遲 n 毫秒")
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     //從Main Activity跳到PokemonListActivity去
-                        //給予指令
                         Intent intent = new Intent(MainActivity.this,PokemonListActivity.class);
                         startActivity(intent);
                 }
             } , changeActivityInSecs * 1000);
         }
     }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+    //選擇夥伴的步驟
+    //1.感受到checked/change
+    //2.儲存選擇的項目
+    //3.回傳選擇的項目,並設定預設值
+    public final static String optionSelectedKey = "selection";
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -91,6 +108,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //UI狀態儲存
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+       //Bundle 的類別 用put放東西 是以key value的形式儲存資料
+        super.onSaveInstanceState(outState);
+        outState.putInt(optionSelectedKey, selectedOptionIndex);
+        //記下使用者所選擇的index
+    }
+
+    //回復使用者所選擇的index
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+                                                                       //預設值為0
+        selectedOptionIndex = savedInstanceState.getInt(optionSelectedKey,0);
+        ((RadioButton)optionGrp.getChildAt(selectedOptionIndex)).setChecked(true);
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////
     //測試不同Activity的功能, 以Log.d測試
     @Override
     protected void onResume() {
@@ -117,24 +153,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public final static String optionSelectedKey = "selection";
-
-    //儲存與回取資料
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-       //Bundle 的類別 用put放東西 是以key value的形式儲存資料
-        super.onSaveInstanceState(outState);
-        outState.putInt(optionSelectedKey, selectedOptionIndex);
-        //記下使用者所選擇的index
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        return false;
     }
-
-    //回復使用者所選擇的index
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-                                                                       //預設值為0
-        selectedOptionIndex = savedInstanceState.getInt(optionSelectedKey,0);
-        ((RadioButton)optionGrp.getChildAt(selectedOptionIndex)).setChecked(true);
-    }
-
 }
