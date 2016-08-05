c364b9f86444f1fc45a0e1cc97222a7e543bea4d
Ooo工友ooO <r00621109@ntu.edu.tw>
2016-08-05 17:53:23 +0800

    將PokemonListActivity 轉換為PokemonListFragment 完成 (包含物件與view的生成 警告視窗的製作 跳轉頁面 Action bar 換成 drawer)

--- app/src/main/java/com/example/user/myapplication/fragment/PokemonListFragment.java
+++ app/src/main/java/com/example/user/myapplication/fragment/PokemonListFragment.java
@@ -3,16 +3,25 @@ package com.example.user.myapplication.fragment;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.user.myapplication.MainActivity;
+import com.example.user.myapplication.PokemonDetailActivity;
 import com.example.user.myapplication.R;
 import com.example.user.myapplication.adapter.PokemonListViewAdapter;
 import com.example.user.myapplication.model.OwningPokemonDataManager;
@@ -24,13 +33,17 @@ import java.util.ArrayList;
  * Created by User on 2016/8/5.
  * 將原本製作的PokemonListActivity轉換成Fragment
  */
-public class PokemonListFragment extends Fragment {
+public class PokemonListFragment extends Fragment implements AdapterView.OnItemClickListener, DialogInterface.OnClickListener{
 
     //宣告變數
     private Activity activity;
-    ArrayList<PokemonInfo> pokemonInfos;
+    private ArrayList<PokemonInfo> pokemonInfos;
     private PokemonListViewAdapter adapter;
     private AlertDialog alertDialog;
+
+    public final static int detailActivityRequestCode = 1;
+    public final static int listRemove = 1;
+    public final static int listLevelup = 2;
 
     //建立一個fragment樣板
     public static PokemonListFragment newInstance(){
@@ -57,7 +70,6 @@ public class PokemonListFragment extends Fragment {
 /*
 * 與UI操作有關 Adapter Setting
 * */
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -75,7 +87,7 @@ public class PokemonListFragment extends Fragment {
         listView.setOnItemClickListener(this);
 
         //Setting Dialog
-        alertDialog = new AlertDialog.Builder(this)
+        alertDialog = new AlertDialog.Builder(activity)
                 .setMessage("你確定要丟棄選取的神奇寶貝嗎?")
                 .setTitle("警告")
                 .setNegativeButton("取消", this)
@@ -90,5 +102,106 @@ public class PokemonListFragment extends Fragment {
     }
 
 
+    //////////////////////////////////////////////////////////////////////////
+    //Dialog 功能開發
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        if (which == AlertDialog.BUTTON_NEGATIVE) {
+            Toast.makeText(activity, "取消丟棄", Toast.LENGTH_SHORT).show();
+        } else if (which == AlertDialog.BUTTON_POSITIVE) {
+            //soundPool.play(pokemon_delete,1,1,0,0,0);
+            //把選取的pokemon info移除
+            for (PokemonInfo pokemonInfo : adapter.selectedPokemon) {
+                adapter.remove(pokemonInfo);
+            }
+            //清除選取的pokemon
+            adapter.selectedPokemon.clear();
+            Toast.makeText(activity, "丟棄完畢", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    /////////////////////////////////////////////////////////////////////////
+    //跳轉頁功能開發
+    //把pokemoninfo拿出來後產生intent
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        PokemonInfo pokemonInfo = adapter.getItem(position);
+        Intent intent = new Intent(activity, PokemonDetailActivity.class);
+        intent.putExtra(PokemonInfo.parcelKey, pokemonInfo);
+        startActivityForResult(intent, detailActivityRequestCode);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == listRemove) {
+
+            String nameToRemove = data.getStringExtra(PokemonInfo.nameKey);
+            PokemonInfo pokemonInfo = adapter.getItemWithName(nameToRemove);
+
+             if (pokemonInfo != null) {
+                adapter.remove(pokemonInfo);
+                Toast.makeText(activity, pokemonInfo.name + "已存入電腦中", Toast.LENGTH_LONG).show();
+            }
+
+        } else if (resultCode == listLevelup) {
+
+            String nameToRemove = data.getStringExtra(PokemonInfo.nameKey);
+            PokemonInfo pokemonInfo = adapter.getItemWithName(nameToRemove);
+
+            if (pokemonInfo != null) {
+                int level = Integer.valueOf(pokemonInfo.level);
+                level += 1;
+                pokemonInfo.level = level;
+                adapter.update(pokemonInfo);
+                Toast.makeText(activity, pokemonInfo.name + "已升級", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    //Action Bar Change to Drawer
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.pokemon_detail_action_bar_menu,menu);
+        return true; //表示一定會顯示action bar的畫面
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_delete) {
+            Log.d("menuItem", "action_delete");
+            if(adapter.selectedPokemon.size()>0) {
+                alertDialog.show();
+            }
+            return true;
+        } else if (itemId == R.id.action_heal) {
+            Log.d("menuItem", "action_heal");
+            //soundPool.play(healing_sound,1,1,0,0,0);
+            //1.抓取被選取的神奇寶貝
+            //2.更新hp資料 = max hp
+            for (PokemonInfo pokemonInfo : adapter.selectedPokemon) {
+                if(pokemonInfo !=null){
+                    if(pokemonInfo.currentHP != pokemonInfo.maxHP) {
+                        pokemonInfo.currentHP = Integer.valueOf(pokemonInfo.maxHP);
+                        adapter.update(pokemonInfo);
+                        Toast.makeText(activity, pokemonInfo.name + "已補血", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(activity, pokemonInfo.name + "滿血中", Toast.LENGTH_SHORT).show();
+                    }
+                    pokemonInfo.isSelected = false;
+                }
+            }
+            //清除選取的pokemon
+            adapter.selectedPokemon.clear();
+
+            return true;
+        } else if (itemId == R.id.action_setting) {
+            Log.d("menuItem", "action_setting");
+            return true;
+        }
+        return false;
+    }
 }
